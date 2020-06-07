3b40ed98bec206b30ed2d923503f25ddcbf7422e
kyk3892 <kyk3892@naver.com>
2020-06-07 19:29:39 +0900

    123
    123

--- src/week12/Seat.java
+++ src/week12/Seat.java
@@ -1,6 +1,8 @@
 package week12;
 
 class Seat{
+	//static String name[]을 하면 공기처럼 모두 공유하는 것이기 때문에
+	//등급별로 달라야 하므로 static있으면 안됨
 	String name[] = {"---","---","---","---","---","---","---","---","---","---"};	
 	public void print() {
 		for(int i=0; i<name.length; i++) {
